ac3df1147123de5317ea23842fde7d703d54ae21
Arafath  Imorou <arafathimorou@gmail.com>
2026-08-10 20:52:24 +0100

    Correction de l'erreur 404 sur les vidéos en autorisant les extensions dans le middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -119,6 +119,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|api/|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|api/|.*\\.(?:svg|png|jpg|jpeg|gif|webp|mp4|mp3|mpg|mpeg|mov|webm)$).*)",
   ],
 };
